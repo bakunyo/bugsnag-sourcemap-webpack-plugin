a177ff97350a1864e426826dd960bbd358b92a48
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-12-27 17:21:11 +0900

    Update plugin src

--- src/BugsnagSourceMapPlugin.js
+++ src/BugsnagSourceMapPlugin.js
@@ -1,3 +1,6 @@
+import async from 'async';
+import fs from 'fs';
+
 class BugsnagSourceMapPlugin {
   apply(compiler) {
     compiler.plugin('after-emit', (compilation) => {
@@ -5,7 +8,23 @@ class BugsnagSourceMapPlugin {
     });
   }
 
-  afterEmit(compilation) {
+  afterEmit(compilation, callback) {
+    const { assetsByChunkName } = compilation.getStats().toJson();
+    const assets = this.getAssets(assetsByChunkName);
+
+    this.uploadSourceMaps(assets, (err) => {
+      callback();
+    });
+  }
+
+  getAssets(assetsByChunkName) {
+    async.filter(assetsByChunkName, (file, callback) => {
+      fs.accessSync(file);
+    })
+  }
+
+  uploadSourceMaps(assets, callback) {
+    callback();
   }
 }
 
